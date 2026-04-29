356b939bc57440fe62cc533f67d758cbb0b07953
Magid Ismail <magidelhadiismail@gmail.com>
2026-04-28 20:37:56 -0700

    fix: remove git submodule, add Xcode project files directly

---
+++ PushToWake/PushToWake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushToWake
+//
+//  Created by Magid Ismail on 2026-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushToWake/PushToWake/PushToWakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushToWakeApp.swift
+//  PushToWake
+//
+//  Created by Magid Ismail on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct PushToWakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
